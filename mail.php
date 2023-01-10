f941731ff3fca55905ded374fd733a4eb77b179f
Guy Radcliffe <gradcliffe@ecfmg.org>
2023-01-10 09:49:27 -0500

    added real email address to  variable

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "gr7432@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
@@ -27,7 +27,7 @@ echo'
     <div class="container">
         <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
         <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
+
     </div>
 </body>
 </html>
